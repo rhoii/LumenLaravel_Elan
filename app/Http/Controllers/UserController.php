88223a524092da20e63c1b98d8e358e7e8b8e1f9
rhoii <rhoydelm@gmail.com>
2025-03-08 00:16:05 +0800

    Final Product

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,66 +4,67 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Response;
-use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
-use DB;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
     private $request;
 
-    public function __construct(Request $request){
+    public function __construct(Request $request)
+    {
         $this->request = $request;
     }
 
-    /* Helper method to return a successful JSON response */
-    protected function successResponse($data, $code = Response::HTTP_OK){
+    protected function successResponse($data, $code = Response::HTTP_OK)
+    {
         return response()->json(['data' => $data], $code);
     }
 
-    /* Helper method to return an error JSON response */
-    protected function errorResponse($message, $code){
+    protected function errorResponse($message, $code)
+    {
         return response()->json(['error' => $message, 'code' => $code], $code);
     }
 
-    /* Return the list of users */
-    public function index(){
+    public function index()
+    {
         $users = User::all();
         return $this->successResponse($users);
     }
 
-    /* Get all users */
-    public function getUsers(){
+    public function getUsers()
+    {
         $users = User::all();
         return $this->successResponse($users);
     }
 
-    /*Add a new user*/
-    public function add(Request $request){
-        $rules = [
-            'username' => 'required|max:20',
-            'password' => 'required|max:20',
-            'gender' => 'required|in:Male,Female',
-        ];
-        
-        $this->validate($request, $rules);
-        $user = User::create($request->all());
-        return $this->successResponse($user, Response::HTTP_CREATED);
-    }
+    public function add(Request $request)
+{
+    $rules = [
+        'username' => 'required|max:20',
+        'password' => 'required|max:20',
+        'gender' => 'required|in:Male,Female',
+    ];
 
-    /*Show details of a single user*/
-    public function show($id){
-        $user = User::find($id);
+    $this->validate($request, $rules);
+    $user = User::create($request->all());
+    return $this->successResponse($user, Response::HTTP_CREATED);
+}
 
-        if (!$user) {
+
+    public function show($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            return $this->successResponse($user);
+        } catch (ModelNotFoundException $e) {
             return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
         }
+    }   
 
-        return $this->successResponse($user);
-    }
-
-    /*Update an existing user*/
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         $rules = [
             'username' => 'max:20',
             'password' => 'max:20',
@@ -71,32 +72,30 @@ class UserController extends Controller
         ];
 
         $this->validate($request, $rules);
-        $user = User::find($id);
 
-        if (!$user) {
+        try {
+            $user = User::findOrFail($id);
+            $user->fill($request->all());
+
+            if ($user->isClean()) {
+                return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            $user->save();
+            return $this->successResponse($user);
+        } catch (ModelNotFoundException $e) {
             return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
         }
-
-        $user->fill($request->all());
-
-        if ($user->isClean()) {
-            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
-
-        $user->save();
-        return $this->successResponse($user);
     }
 
-    /* Delete a user */
     public function delete($id)
     {
-        $user = User::find($id);
-
-        if (!$user) {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return $this->successResponse(['message' => 'User deleted successfully']);
+        } catch (ModelNotFoundException $e) {
             return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
         }
-
-        $user->delete();
-        return $this->successResponse(['message' => 'User deleted successfully']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,16 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+// Unsecured Routes
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users',['uses' => 'UserController@getUsers']);
+    $router->get('/users', 'UserController@index'); // Get all users
+    $router->get('/users/{id}', 'UserController@show'); // Get user by ID
+    $router->post('/users', 'UserController@add'); // Create a new user
+    $router->put('/users/{id}', 'UserController@update'); // Update user
+    $router->patch('/users/{id}', 'UserController@update'); // Partial update
+    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
 });
-
-// more simple routes
-$router->get('/users',['uses' => 'UserController@getUsers']);
-$router->get('/users', 'UserController@index');   // get all users 
-$router->post('/users', 'UserController@add');  // create new user 
-$router->get('/users/{id}', 'UserController@show'); // get user by id
-$router->put('/users/{id}', 'UserController@update'); // update user 
-$router->patch('/users/{id}', 'UserController@update'); // update user 
-$router->delete('/users/{id}', 'UserController@delete'); // delete 
