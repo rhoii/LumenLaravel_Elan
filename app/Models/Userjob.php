577dc76ffd886cd2f55072c4fd9c9b65de3c25eb
rhoii <rhoydelm@gmail.com>
2025-04-13 00:17:58 +0800

    userjob

--- app/Models/Userjob.php
+++ app/Models/Userjob.php
@@ -8,11 +8,6 @@ class UserJob extends Model
 {
     protected $table = 'usertablejob';
     protected $primaryKey = 'jobid';
-    public $timestamps = true; 
-    protected $fillable = ['jobid', 'jobname']; 
-    // Define the relationship with the User model (one-to-many)
-    public function users()
-    {
-        return $this->hasMany(User::class, 'jobid');
-    }
+    protected $fillable = ['jobid', 'jobname'];
+    public $timestamps = false;
 }
